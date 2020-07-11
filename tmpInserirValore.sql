8386a877aef5b1e066faaecaaa0b6e140101c9b5
pedrorsvalle <pedrorsvalle@gmail.com>
2020-07-11 20:09:41 -0300

    Adiciona valor a tabela historico

--- tmpInserirValore.sql
+++ tmpInserirValore.sql
@@ -1,7 +1,2 @@
-insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP) values 
-('Vicente Raimundo Gustavo da Rocha','Rua Ipê-branco','M','Dunas do Peró','    ','22\/11\/1959','(22) 2731-8445','(22) 99971-5982 '),
- ('Mariane Alícia da Silva','Rua Coronel Alberto de Melo','F','Vila de Cava','    ','26\/08\/1946','(21) 3681-3041','(21) 99344-6230'),
- ('Caroline Sophia Dias','Rua Alberto Domingos','F','Triângulo','    ','14\/01\/1980','(24) 2729-3362 ','(24) 98785-6897 '),
-('Eliane Clarice Marina das Neves','Rua Maria Adelaide de Carvalho','F','Centro','    ','10\/08\/1954','(21) 3874-1216 ','(21) 98887-6552 '),
- ('Osvaldo Carlos Anthony Mendes','Travessa Luzia Cruz','M ','Parque Leopoldina','       ','07\/12\/1999 ','(22) 2770-7743 ','(22) 99860-9751'),
-('Malu Vanessa Gabriela Araújo','Rua José Eugênio',' F','São Cristóvão ','     ','26\/07\/1978','(21) 3792-7948','(21) 99150-7818');
+INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecedentesCirurgicos) VALUES
+(1, 1, "apendicite")
