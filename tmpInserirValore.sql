9b47065012fff7eff802b4b46ddac34ad29fef1a
Pedro Rodrigues Santos Valle <54651078+pedrorsvalle@users.noreply.github.com>
2020-07-12 12:34:51 -0300

    Update tmpInserirValore.sql

--- tmpInserirValore.sql
+++ tmpInserirValore.sql
@@ -31,4 +31,17 @@ insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento
 insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Malu Vanessa Gabriela Araújo','Rua José Eugênio',' F','São Cristóvão','09/08/2003','26\/07\/1978','(21) 3792-7948','(21) 99150-7818',' ',' ','',' ',' ',' ','  ','  ','  ','  ',' ',' ',' ','  ');
 
 ######################################################################################################################################################################################################################################
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values 
+ ('Vicente Raimundo Gustavo da Rocha','Rua Ipê-branco','M','Dunas do Peró','05/02/2000','22\/11\/1959','(22) 2731-8445','(22) 99971-5982',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','');
+ ('Mariane Alícia da Silva','Rua Coronel Alberto de Melo','F','Vila de Cava','03/04/2007','26\/08\/1946','(21) 3681-3041','(21) 99344-6230',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ',' ');
+ ('Caroline Sophia Dias','Rua Alberto Domingos','F','Triângulo','02/07/2005','14\/01\/1980','(24) 2729-3362 ','(24) 98785-6897',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','  ');
+ ('Eliane Clarice Marina das Neves','Rua Maria Adelaide de Carvalho','F','Centro','06/07/2002','10\/08\/1954','(21) 3874-1216 ','(21) 98887-6552',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ',' ');
+ ('Osvaldo Carlos Anthony Mendes','Travessa Luzia Cruz','M ','Parque Leopoldina','03/04/2018','07\/12\/1999 ','(22) 2770-7743 ','(22) 99860-9751',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','');
+ ('Malu Vanessa Gabriela Araújo','Rua José Eugênio',' F','São Cristóvão','09/08/2003','26\/07\/1978','(21) 3792-7948','(21) 99150-7818',' ',' ','',' ',' ',' ','  ','  ','  ','  ',' ',' ',' ','  ');
+
+
+
+
+
+------------------------------------------------------------------------------------------------
 Falta colocar o final dessa Tabela CEP até idhistorico
