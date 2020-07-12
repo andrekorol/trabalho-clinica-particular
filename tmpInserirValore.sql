55da84a9792aaa51aa694066996d3db6fa9318eb
Pedro Rodrigues Santos Valle <54651078+pedrorsvalle@users.noreply.github.com>
2020-07-12 11:02:26 -0300

    Update tmpInserirValore.sql

--- tmpInserirValore.sql
+++ tmpInserirValore.sql
@@ -2,12 +2,7 @@ INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecede
 (1, 1, "apendicite"),
 (0, 0, "-");
 
-
-
-
-
-
-
+###############################################################################################
 INSERT INTO historico(permaneceMuitoSentado,temAntecedentesCirurgicos,antecedentesCirurgicos,
 fezTratamentoEsteicoAnteriormente,tratamentoEsteticoAnterior,temAtencendetesAlergicos,
 atecendentesAlergicos,praticaAtividadeFsica,atividadesFisicas,
@@ -26,3 +21,14 @@ hipertensao,epilepisia,temHilepisia,hipotensao,diabetes)
 VALUES(1,1,"apendice"),(0, "dremagem Linfatica" 1 ),("caramarao" ,1, futebol),(1,1,"fastfood"),(1,"cerveja",1),(1,3,1),
 ("psiquiatra",1,"acidoHialuronico"),(1,"suplementos,1"),(0,"umAparelho",1),("ferro"),(1,"neoplasias"),(1,"dura5dias",1),
 ("prevencao",1," vasinhos"),(1,"Tornozelo e Pé"),(1,0,"doencanoSistemaNervoso"),(1,1);
+###########################################################################################################################
+
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Vicente Raimundo Gustavo da Rocha','Rua Ipê-branco','M','Dunas do Peró','05/02/2000','22\/11\/1959','(22) 2731-8445','(22) 99971-5982',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','');
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Mariane Alícia da Silva','Rua Coronel Alberto de Melo','F','Vila de Cava','03/04/2007','26\/08\/1946','(21) 3681-3041','(21) 99344-6230',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ',' ');
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Caroline Sophia Dias','Rua Alberto Domingos','F','Triângulo','02/07/2005','14\/01\/1980','(24) 2729-3362 ','(24) 98785-6897',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','  ');
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Eliane Clarice Marina das Neves','Rua Maria Adelaide de Carvalho','F','Centro','06/07/2002','10\/08\/1954','(21) 3874-1216 ','(21) 98887-6552',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ',' ');
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Osvaldo Carlos Anthony Mendes','Travessa Luzia Cruz','M ','Parque Leopoldina','03/04/2018','07\/12\/1999 ','(22) 2770-7743 ','(22) 99860-9751',' ',' ','',' ',' ',' ','  ','   ','  ','  ',' ','  ',' ','');
+insert into anamneseCorporal(nome,Endereço,sexo,bairro,dataFicha,dataNascimento,FonesRes,FonesComercial,CEP,etinia,profissao,email,motivoDaVisita,nomeEmergencia,telefoneEmergencia,nomeMedico,telefoneMedico,convenioMedico,carterinhaConvenio,hosptital,medico_idmedico,historico_idhistorico) values ('Malu Vanessa Gabriela Araújo','Rua José Eugênio',' F','São Cristóvão','09/08/2003','26\/07\/1978','(21) 3792-7948','(21) 99150-7818',' ',' ','',' ',' ',' ','  ','  ','  ','  ',' ',' ',' ','  ');
+
+######################################################################################################################################################################################################################################
+Falta colocar o final dessa Tabela CEP até idhistorico
