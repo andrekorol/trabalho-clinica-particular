14ada0250d74a89c72c2b9b261fc7eb889a02bc5
Pedro Rodrigues Santos Valle <54651078+pedrorsvalle@users.noreply.github.com>
2020-07-12 10:37:23 -0300

    Update tmpInserirValore.sql

--- tmpInserirValore.sql
+++ tmpInserirValore.sql
@@ -1,7 +1,14 @@
 INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecedentesCirurgicos) VALUES
 (1, 1, "apendicite"),
 (0, 0, "-");
-INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecedentesCirurgicos,
+
+
+
+
+
+
+
+INSERT INTO historico(permaneceMuitoSentado,temAntecedentesCirurgicos,antecedentesCirurgicos,
 fezTratamentoEsteicoAnteriormente,tratamentoEsteticoAnterior,temAtencendetesAlergicos,
 atecendentesAlergicos,praticaAtividadeFsica,atividadesFisicas,
 fumante,temAlimentacaoBalanceada,alimentacao,
@@ -16,4 +23,6 @@ temCicloMenstrualRegular,observacoesCicloMestrual,
 usaAnticoncepiciona,metodoAnticoncepicional,temVarizes,
 graVarizes,temLesoes,tipoDeLesoes,
 hipertensao,epilepisia,temHilepisia,hipotensao,diabetes)
-VALUES(1,1,"apendice"),(0, "dremagem Linfatica" 1 ),("caramarao" ,1, pao);
+VALUES(1,1,"apendice"),(0, "dremagem Linfatica" 1 ),("caramarao" ,1, futebol),(1,1,"fastfood"),(1,"cerveja",1),(1,3,1),
+("psiquiatra",1,"acidoHialuronico"),(1,"suplementos,1"),(0,"umAparelho",1),("ferro"),(1,"neoplasias"),(1,"dura5dias",1),
+("prevencao",1," vasinhos"),(1,"Tornozelo e Pé"),(1,0,"doencanoSistemaNervoso"),(1,1);
