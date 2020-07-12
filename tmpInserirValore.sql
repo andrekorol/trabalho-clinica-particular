0e7dcd7cef48bcd135aa268405c0dba4fa2cb19a
Pedro Rodrigues Santos Valle <54651078+pedrorsvalle@users.noreply.github.com>
2020-07-12 00:09:55 -0300

    Update tmpInserirValore.sql

--- tmpInserirValore.sql
+++ tmpInserirValore.sql
@@ -1,3 +1,19 @@
 INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecedentesCirurgicos) VALUES
 (1, 1, "apendicite"),
 (0, 0, "-");
+INSERT INTO historico(permaneceMuitoSentado, temAntecedentesCirurgicos, antecedentesCirurgicos,
+fezTratamentoEsteicoAnteriormente,tratamentoEsteticoAnterior,temAtencendetesAlergicos,
+atecendentesAlergicos,praticaAtividadeFsica,atividadesFisicas,
+fumante,temAlimentacaoBalanceada,alimentacao,
+bebeliquidosComFrequencia,liquidos,gestante,
+temFilhos,numeroFilhos,algumProblemaOrtopédico,
+problemaOrtopédico,algumTratamentoMedico,usouAcidosNaPele,
+acidosUsados,algumTratamentoOrtomecular,
+tratamentoOrtomcular,temCuidadoDiarios,cuidadosDiarios,
+portadorDeAmarcapasso,Marcapasso,presencaeMetais,
+metais,temAtencedentesOncalogicos,antecedentesOncologicos,
+temCicloMenstrualRegular,observacoesCicloMestrual,
+usaAnticoncepiciona,metodoAnticoncepicional,temVarizes,
+graVarizes,temLesoes,tipoDeLesoes,
+hipertensao,epilepisia,temHilepisia,hipotensao,diabetes)
+VALUES(1,1,"apendice"),(0, "dremagem Linfatica" 1 ),("caramarao" ,1, pao);
